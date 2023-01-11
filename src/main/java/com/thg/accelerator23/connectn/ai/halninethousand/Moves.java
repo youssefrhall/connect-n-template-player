b887cd29f797d378e72870407dddd605f866d379
Drew <drew.soul61@thehutgroup.com>
2023-01-11 09:31:40 +0000

    win check updated

--- src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/Moves.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/Moves.java
@@ -4,7 +4,7 @@ import com.thehutgroup.accelerator.connectn.player.Counter;
 
 public class Moves {
 
-    public int winFinder (Arena arena, Counter counter){
+    public static int winFinder(Arena arena, Counter counter){
         int[] moveHeight = arena.getHeighestCounter();
         for (int column = 0; column < 10; column++){
             arena.setCounter(column, moveHeight[column], counter);
@@ -17,19 +17,18 @@ public class Moves {
         return 11;
     }
 
-    private boolean horizontalWin(Arena arena, Counter counter, int column, int[] moveHeight){
-            for (int offset = 0; offset < 3; offset++){
-                if (column - offset >= 0 &&  column-offset+3 < 10)
+    private static boolean horizontalWin(Arena arena, Counter counter, int column, int[] moveHeight){
+            for (int offset = 0; offset <= 3; offset++){
+                if (column - offset >= 0 &&  column-offset+3 < 10){
             if (arena.getCounter(column - offset, moveHeight[column]) == counter &&  arena.getCounter(column+1 - offset, moveHeight[column]) == counter && arena.getCounter(column+2-offset, moveHeight[column]) == counter && arena.getCounter(column+3-offset, moveHeight[column]) == counter) {
                 return true;
-
             }
+                }
         }
         return false;
     }
 
-    private boolean verticalWin(Arena arena, Counter counter, int column, int[] moveHeight){
-
+    private static boolean verticalWin(Arena arena, Counter counter, int column, int[] moveHeight){
             if (arena.getHeighestCounter()[column]>1) {
                 if (arena.getCounter(column, moveHeight[column]) == counter && arena.getCounter(column, moveHeight[column] - 1) == counter && arena.getCounter(column, moveHeight[column] - 2) == counter) {
                     return true;
@@ -39,7 +38,7 @@ public class Moves {
         return false;
     }
 
-    private boolean diagonalDownWin(Arena arena, Counter counter, int column, int[] moveHeight){
+    private static boolean diagonalDownWin(Arena arena, Counter counter, int column, int[] moveHeight){
 
             for (int offset = 0; offset < 3; offset++){
             if (arena.getHeighestCounter()[column]>2 && column - offset >= 0 &&  column-offset+3 < 10) {
@@ -52,10 +51,10 @@ public class Moves {
         return false;
     }
 
-    private boolean diagonalUpWin(Arena arena, Counter counter, int column, int[] moveHeight){
+    private static boolean diagonalUpWin(Arena arena, Counter counter, int column, int[] moveHeight){
 
             for (int offset = 0; offset < 3; offset++){
-            if (arena.getHeighestCounter()[column]<7 & column - offset >= 0 &&  column-offset+3 < 10) {
+            if (arena.getHeighestCounter()[column]<7 && column - offset >= 0 &&  column-offset+3 < 10 && moveHeight[column]-offset >= 0 && moveHeight[column]-offset < 10) {
                 if (arena.getCounter(column - offset, moveHeight[column] - offset) == counter && arena.getCounter(column + 1 - offset, moveHeight[column] + 1 - offset) == counter && arena.getCounter(column + 2 - offset, moveHeight[column] + 2 - offset) == counter && arena.getCounter(column + 3 - offset, moveHeight[column] + 3 - offset) == counter) {
                     return true;
                 }
