2f6a918ac31cf109309d2cbe4f4d2c11f8627663
youssefrhall <youssef.hall43@thehutgroup.com>
2023-01-11 16:12:54 +0000

    MiniMax works

--- src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/MiniMax/MiniMax.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/MiniMax/MiniMax.java
@@ -1,34 +1,87 @@
 package com.thg.accelerator23.connectn.ai.halninethousand.MiniMax;
+import com.thehutgroup.accelerator.connectn.player.*;
+import com.thg.accelerator23.connectn.ai.halninethousand.MiniMax.BoardAnalysis.BoardAnalyser;
+import com.thg.accelerator23.connectn.ai.halninethousand.MiniMax.BoardAnalysis.Node;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MiniMax {
-    public int miniMaxWithAlphaBetaPruning(int node,int depth, boolean maximizingPlayer, int alpha, int beta){
-        if(depth == 40){
-            //System.out.println(values[node]);
-            System.out.println(node);
-            return node;
+    private int move;
+    public int miniMaxWithAlphaBetaPruning(Board board, int depth, boolean maximizingPlayer, int alpha, int beta) {
+        BoardAnalyser boardAnalyser = new BoardAnalyser(new GameConfig(10,8,4));
+        if(depth == 10 || boardAnalyser.calculateGameState(board).isEnd()){
+            if(!board.hasCounterAtPosition(new Position(4,0))){
+                return 10000;
+            }if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.O) == 4){
+                return 1000;
+            }else if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.O) == 3){
+                return 10;
+            } else if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.O) == 2) {
+                return 5;
+            }else if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.X) == 2){
+                return -5;
+            }if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.X) == 3){
+                return -10;
+            }if(boardAnalyser.calculateGameState(board).getMaxInARowByCounter().get(Counter.X) == 4){
+                return -1000;
+            }
+            else{
+                return 0;
+            }
         }
+        int bestMove = -1;
         if(maximizingPlayer){
-            int maxValue = -1000;
-            for(int i = 0; i < 2; i++){
-                int score = miniMaxWithAlphaBetaPruning(node * 2 + i,depth + 1, false, alpha, beta);
-                maxValue = Math.max(maxValue, score);
+            int maxValue = Integer.MIN_VALUE;
+            for(int i = 0; i < 10; i++){
+                if(board.hasCounterAtPosition(new Position(i,7))){
+                    continue;
+                }
+                try{
+                    board = new Board(board, i, maximizingPlayer ? Counter.O : Counter.X);
+                }catch (InvalidMoveException e){
+                    //System.out.println(e.getMessage());
+                }
+                int score = miniMaxWithAlphaBetaPruning(board,depth + 1, false, alpha, beta);
+                if (score > maxValue) {
+                    maxValue = score;
+                    bestMove = i;
+                }
                 alpha = Math.max(alpha, maxValue);
                 if(beta <= alpha){
                     break;
                 }
             }
-            return maxValue;
+            if(bestMove == -1){
+                return maxValue;
+            }
+            return bestMove;
         }else{
-            int minvalue = 1000;
-            for(int i = 0; i < 2; i++){
-                int score = miniMaxWithAlphaBetaPruning(node * 2 + i,depth + 1, true, alpha, beta);
-                minvalue = Math.min(minvalue, score);
-                beta = Math.min(beta, minvalue);
+            int minValue = Integer.MAX_VALUE;
+            for(int i = 0; i < 10; i++){
+                if(board.hasCounterAtPosition(new Position(i,7))){
+                    continue;
+                }
+                try{
+                    board = new Board(board, i, maximizingPlayer ? Counter.X : Counter.O);
+                }catch (InvalidMoveException e){
+                    //System.out.println(e.getMessage());
+                }
+                int score = miniMaxWithAlphaBetaPruning(board,depth + 1, true, alpha, beta);
+                if (score < minValue) {
+                    minValue = score;
+                    bestMove = i;
+                }
+
+                beta = Math.min(beta, minValue);
                 if(beta <= alpha){
                     break;
                 }
             }
-            return minvalue;
+            if(bestMove == -1){
+                return minValue;
+            }
+            return bestMove;
         }
     }
 }
--- src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/Minimax.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/halninethousand/Minimax.java
@@ -1,15 +1,23 @@
 package com.thg.accelerator23.connectn.ai.halninethousand;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
-import com.thehutgroup.accelerator.connectn.player.GameConfig;
+import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.Player;
 import com.thg.accelerator23.connectn.ai.halninethousand.MiniMax.BoardAnalysis.Node;
 import com.thg.accelerator23.connectn.ai.halninethousand.MiniMax.MiniMax;
 
-public class main {
-    public static void main(String[] args) {
+
+public class Minimax extends Player {
+    public Minimax(Counter counter) {
+        //TODO: fill in your name here
+        super(counter, Four0.class.getName());
+    }
+
+    @Override
+    public int makeMove(Board board) {
         MiniMax miniMax = new MiniMax();
-        Board board = new Board(new GameConfig(10,8,4));
         int finalValue = miniMax.miniMaxWithAlphaBetaPruning(board,0,true,-1000,1000);
         System.out.println(finalValue);
+        return finalValue;
     }
 }
